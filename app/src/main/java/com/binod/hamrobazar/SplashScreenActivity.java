a4ef0d3f0421ec37aad5715adfcbd0885b0ee411
Binodmagar <ranabinod615@gmail.com>
2020-01-02 21:11:48 +0545

    [#4] splash screen activity finished along with design

--- app/src/main/java/com/binod/hamrobazar/SplashScreenActivity.java
+++ app/src/main/java/com/binod/hamrobazar/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package com.binod.hamrobazar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        //using a thread and halt screen for 2 seconds
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        },1000);
     }
 }
