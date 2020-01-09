76be868b5a1ff0be65b6af369dabc3dc3214164f
Binodmagar <ranabinod615@gmail.com>
2020-01-09 14:42:45 +0545

    [#7] login form design finished

---
+++ app/src/main/java/com/binod/model/UserLogin.java
@@ -0,0 +1,128 @@
+package com.binod.model;
+
+public class UserLogin {
+
+    private String fullName;
+    private String email;
+    private String password;
+    private String phone;
+    private String mobilePhone;
+    private String street;
+    private String area;
+    private String city;
+    private Boolean newsletter;
+    private Boolean hidePhone;
+    private Boolean agree;
+    private String image;
+
+    public UserLogin(String fullName, String email, String password, String phone, String mobilePhone, String street, String area, String city, Boolean newsletter, Boolean hidePhone, Boolean agree, String image) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.mobilePhone = mobilePhone;
+        this.street = street;
+        this.area = area;
+        this.city = city;
+        this.newsletter = newsletter;
+        this.hidePhone = hidePhone;
+        this.agree = agree;
+        this.image = image;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Boolean getNewsletter() {
+        return newsletter;
+    }
+
+    public void setNewsletter(Boolean newsletter) {
+        this.newsletter = newsletter;
+    }
+
+    public Boolean getHidePhone() {
+        return hidePhone;
+    }
+
+    public void setHidePhone(Boolean hidePhone) {
+        this.hidePhone = hidePhone;
+    }
+
+    public Boolean getAgree() {
+        return agree;
+    }
+
+    public void setAgree(Boolean agree) {
+        this.agree = agree;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
